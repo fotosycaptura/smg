66e78290042b6f6254d9a0c7cae1e515cf653617
fotosycaptura <charsxavier@gmail.com>
2023-06-06 21:23:27 -0400

    Se agrega nueva url

--- smg.py
+++ smg.py
@@ -2,9 +2,9 @@ import configparser
 import json
 import sys, getopt
 from varios import utilerias
-from scrap import submanga_io, leermanga_net
+from scrap import submanga_io, leermanga_net, manhwas_net
 """
-Creado en python 3.10
+Creado en python 3.11
 """
 
 """
@@ -14,7 +14,7 @@ Esta seccion es para la configuracion interna del programa
 smg_Lib, es la ruta o librería en donde están almacenados los mangas.
 Está definida en config.ini en el parámetro ruta_mangas
 """
-smg_version = "1.3.2"
+smg_version = "1.3.3"
 config = configparser.ConfigParser()
 config.read('config.ini') 
 home_mangas = config['DEFAULT']['ruta_mangas']
@@ -42,6 +42,9 @@ def verifica_argumentos(argv):
                         elif(args[0].startswith("https://leermanga.net/manga/")):
                                 leerManga = leermanga_net.leermanga_net(smg_version, home_mangas, opciones, listado_mangas)
                                 leerManga.descargar_solo(home_mangas, args[0])
+                        elif(args[0].startswith('https://www.manhwas.net/manga/')):
+                                leer_manhwas = manhwas_net.manhwas_net(smg_version, home_mangas, opciones, listado_mangas)
+                                leer_manhwas.descargar_solo(args[0])
         except getopt.GetoptError:
                 print('smg.py -h para mayor información')
                 sys.exit(2)
